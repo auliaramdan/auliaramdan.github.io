d7b544259714ec70a07482a5d99ddd7ecb3eaa02
auliaramdan <32842840+auliaramdan@users.noreply.github.com>
2025-09-15 13:32:29 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl"
 
 export default {
-    base: 'auliaramdan.github.io/src',
+    base: '/src',
     outDir: './../dist',
-    publicDir: './src/res',
+    publicDir: '/src/res',
     plugins: [
         viteBasicSslPlugin()
     ]
